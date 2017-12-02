888a420405f9954468336495502eb2ec24cb50b6
lemones <citronster@gmail.com>
2017-12-02 23:36:41 +0100

    02 Nov 2017

--- bin/wallmenu.sh
+++ bin/wallmenu.sh
@@ -10,58 +10,54 @@ function show_menu {
     $(tput sgr0)
               Wallpaper browser.
 
-    Press left or right to change wallpaper.
+    Press n to cycle next wallpaper.
     Press s to set or q to undo and quit.
 
     "
 
     if [[ $backup_done != False ]]; then
         check_file; fi
-    read_keys
+    change_w
 }
 
 function check_file {
-    readf=`ls -1 $HOME/.wallpaper{.jpg,.png,.JPG,.PNG,.jpeg,.JPEG} 2>/dev/null | wc -l`
-    if [ $readf != 0 ]; then
-        if_file_exists
+    filen_check=`ls -1 $HOME/.wallpaper{.jpg,.png,.JPG,.PNG,.jpeg,.JPEG} 2>/dev/null | wc -l`
+    filen=`ls -1 $HOME/.wallpaper{.jpg,.png,.JPG,.PNG,.jpeg,.JPEG} 2>/dev/null`
+    if [ $filen_check != 0 ]; then
+        echo "Saving old wallpaper as $filen-backup..."
+        mv $filen $filen-backup
+        backup_fil=$filen
+        backup_done=True
     else
-        if_file_missing
+        backup_done=False
     fi
-}
-
-function if_file_exists {
-    echo "File exists... Taking backup."
-    mv $HOME/.wallpaper.jpg $HOME/.wallpaper.jpg-backup
-    backup_done=True
-}
-
-function if_file_missing {
-    echo "File missing... press to continue"
-    backup_done=False
 }
 
 function change_w {
     WP="$HOME/Pictures/wallpapers/*"
     for file in $WP; do
-    	#feh --bg-fill $file
-    	echo -e "$file"
+        read -rsn1 key
+        if [ "$key" = 's' ]; then
+            echo "Setting $file as bg..."
+            cp $file $HOME/.wallpaper.png
+            feh --bg-scale $HOME/.wallpaper.png
+            exit
+        elif [ "$key" = 'q' ]; then
+            if [ -e "$backup_fil-backup" ]; then
+                echo "Reverting to last known wallpaper..."
+                mv $backup_fil-backup $backup_fil
+                feh --bg-scale $backup_fil
+            else
+                echo "No backup file found...Sorry :("
+            fi
+            exit
+        elif [ "$key" = 'n' ]; then
+            feh --bg-scale $file
+            continue
+        else
+            echo -e "s - Set | q - quit | n - next"
+        fi
     done
-    show_menu
 }
 
-function read_keys {
-    escape_char=$(printf "\u1b")
-    read -rsn1 mode
-
-    if [[ $mode == $escape_char ]]; then
-        read -rsn2 mode
-    fi
-
-    case $mode in
-        'q') echo "Exit..." ; exit ;;
-        '[D') change_w ;;
-        '[C') change_w ;;
-        *) >&2 echo 'ERR bad input'; show_menu ;;
-    esac
-}
 show_menu
